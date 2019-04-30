aa437aadafdfcd9fc418455f5a340da2d6174b77
Andres Urquijo <andyurquijo@gmail.com>
2019-04-29 21:27:28 -0500

    Imported TextMeshPro. Setup UI.

--- MadbricksTest/Assets/_Scripts/GameMaster.cs
+++ MadbricksTest/Assets/_Scripts/GameMaster.cs
@@ -1,18 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
 
 public class GameMaster : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+
     void Start()
     {
-        
+        PlayFabSettings.TitleId = "39114";
+
+     }
+
+    public void Login()
+    {
+        var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnLoginSuccess(LoginResult result)
     {
-        
+        Debug.Log("Congratulations, you made your first successful API call!");
+    }
+
+    private void OnLoginFailure(PlayFabError error)
+    {
+        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogError("Here's some debug information:");
+        Debug.LogError(error.GenerateErrorReport());
     }
 }
