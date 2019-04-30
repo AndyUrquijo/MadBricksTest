13178ab735a32c438d31cd8b81124c4aba70c907
Andres Urquijo <andyurquijo@gmail.com>
2019-04-29 23:03:35 -0500

    Setup Playfab logic for login, load and save. Added a ui extension for UI tabbing.

--- MadbricksTest/Assets/_Scripts/GameMaster.cs
+++ MadbricksTest/Assets/_Scripts/GameMaster.cs
@@ -3,32 +3,90 @@ using System.Collections.Generic;
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
+using UnityEngine.UI;
+using System;
 
 public class GameMaster : MonoBehaviour
 {
+    readonly string DataKey = "Data";
 
+    [SerializeField] TMPro.TMP_InputField LoginInput = null;
+    [SerializeField] TMPro.TMP_InputField DataInput = null;
+    [SerializeField] Button LoginButton = null;
+    [SerializeField] Button SaveButton = null;
+
+    string PlayfabId = null;
 
     void Start()
     {
         PlayFabSettings.TitleId = "39114";
-
-     }
+        DataInput.interactable = false;
+        SaveButton.interactable = false;
+    }
 
     public void Login()
     {
-        var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
-        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+        ActivateElements(false);
+
+        var user = LoginInput.text;
+        var request = new LoginWithCustomIDRequest { CustomId = user, CreateAccount = true };
+        PlayFabClientAPI.LoginWithCustomID(request,
+            (result) =>
+            {
+                Debug.Log("Logged in");
+                PlayfabId = result.PlayFabId;
+                Load();
+            },
+            OnError);
     }
 
-    private void OnLoginSuccess(LoginResult result)
+    void Load()
     {
-        Debug.Log("Congratulations, you made your first successful API call!");
+        var request = new GetUserDataRequest();
+        PlayFabClientAPI.GetUserData(request,
+            (result) =>
+            {
+                Debug.Log("Data obtained");
+                if (result.Data.TryGetValue(DataKey, out UserDataRecord dataRecord))
+                    DataInput.text = dataRecord.Value;
+                else
+                    DataInput.text = "";
+                ActivateElements(true);
+            },
+            OnError);
     }
 
-    private void OnLoginFailure(PlayFabError error)
+    public void Save()
     {
-        Debug.LogWarning("Something went wrong with your first API call.  :(");
-        Debug.LogError("Here's some debug information:");
-        Debug.LogError(error.GenerateErrorReport());
+        ActivateElements(false);
+
+        var request = new UpdateUserDataRequest();
+
+        request.Data = new Dictionary<string, string>();
+        request.Data.Add(DataKey, DataInput.text);
+
+        PlayFabClientAPI.UpdateUserData(request,
+            (result) =>
+            {
+                Debug.Log("Data saved");
+                ActivateElements(true);
+            },
+            OnError);
+    }
+
+    void OnError(PlayFabError error)
+    {
+        Debug.Log("Playfab Error");
+        Debug.LogWarning(error.GenerateErrorReport());
+        ActivateElements(true);
+    }
+
+    void ActivateElements(bool active)
+    {
+        LoginInput.interactable = active;
+        LoginButton.interactable = active;
+
+        DataInput.interactable = active && PlayfabId != null;
+        SaveButton.interactable = active && PlayfabId != null;
     }
 }
